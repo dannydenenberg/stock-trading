d71bb3c9911e98f2f2d6a10d73ed1235ea5ed31a
Danny Denenberg <dannydenenberg@gmail.com>
2021-05-20 13:01:18 -0500

    done with /buy. need to finish /sell

--- db.js
+++ db.js
@@ -1,8 +1,69 @@
 var crypto = require("crypto");
+const { v4: uuidv4 } = require("uuid");
+const yahooFinance = require("yahoo-finance");
 
 // database for now.
 // TODO: add mongo
-let database = [];
+let database = [
+  {
+    username: "zach",
+    // 123
+    password:
+      "3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2",
+    historyOfMoney: [],
+    money: 10467,
+    transactions: [],
+    holdings: [],
+  },
+];
+/**
+ * Person in Database:
+{
+  username,
+  password,
+  // history of adding/taking away money from the account
+  historyOfMoney: [
+    {
+      date: 2893467882,
+      moneyAdded: -23984 // positive or negative number for how much added/subtracted
+    }
+  ]
+  money: 128374.38,
+  netWorth: [{date: 8273874, worth: 173872}, {date: 923843, worth: 1829387}] // updated periodically
+  transactions: [
+    {
+      transactionId: "UUID",
+      stocks: [
+        {
+          pricePerShare: 3829.32,
+          ticker: "VTI",
+          quantity: 4,
+          date: 38298473
+        }
+      ]
+    }
+  ],
+  holdings: [
+    {
+      holdingId: "UUID",
+      ticker: "VTI", 
+      quantity: 4
+    },
+    {
+      holdingId: "UUID",
+      ticker: "aapl",
+      quantity: 5,
+      limit: 249.43
+    },
+    {
+      holdingId: "UUID",
+      ticker: "brk-a",
+      quantity: 2,
+      stop: 200000
+    }
+  ]
+}
+ */
 
 /**
  * Creates a new person in the database.
@@ -17,7 +78,15 @@ module.exports.createPerson = (username, password, callback) => {
   } else {
     // create person
     let hashedPassword = hash(password);
-    database.push({ username, password: hashedPassword });
+    let newPerson = {
+      username,
+      password: hashedPassword,
+      money: 0,
+      historyOfMoney: [],
+      transactions: [],
+      holdings: [],
+    };
+    database.push(newPerson);
     // console.log(database);
     callback(null); // no error
   }
@@ -43,20 +112,132 @@ module.exports.passwordIsCorrect = (username, password, callback) => {
 };
 
 /**
- *
+ * Takes a person's cart and
+ * - adds cart to holdings
+ * - subtracts total amount from the person's money
+ * - update TRANSACTIONS
+ * @param {*} username
+ * @param {*} cart -- already has the cart[i].currentPriceOfOneShare
+ * @param {*} callback(err)
+ */
+module.exports.completePurchase = (username, cart, totalPrice, callback) => {
+  for (let i = 0; i < cart.length; i++) {
+    cart[i].date = new Date();
+  }
+  for (let i = 0; i < database.length; i++) {
+    if (database[i].username == username) {
+      // it's the person
+
+      // update money
+      database[i].money -= totalPrice;
+
+      // update holdings
+      database[i].holdings = database[i].holdings.concat(cart);
+
+      // update transactions
+      database[i].transactions.push({
+        transactionId: uuidv4(),
+        date: new Date(),
+        stocks: cart,
+      });
+
+      // send callback
+      callback(null);
+    }
+  }
+
+  console.log(database);
+};
+
+/**
+ * Send back person given a username.
+ * Add the current stock prices to their holdings.
  * @param {*} username
  * @param {*} callback (err, person)
  */
-module.exports.getPerson = (username, callback) => {
+module.exports.getPerson = async (username, callback) => {
+  // TODO: add currentPrice to the person.holdings elements using Yahoo Finance.
   let list = database.filter((person) => person.username == username);
   if (list.length == 1) {
     let person = list[0];
+
+    for (let i = 0; i < person.holdings.length; i++) {
+      try {
+        person.holdings[i].currentPriceOfOneShare = await getQuote(
+          person.holdings[i].ticker,
+        );
+      } catch (err) {
+        console.log(err);
+        person.holdings[i].currentPriceOfOneShare = 0;
+      }
+    }
+
     callback(null, person);
   } else {
     callback(true, null);
   }
 };
 
+module.exports.addOrSubtractMoney = (
+  username,
+  addOrSubtract,
+  amount,
+  callback,
+) => {
+  let list = database.filter((person) => person.username == username);
+  if (list.length == 1) {
+    if (addOrSubtract == "subtract") {
+      amount = -amount;
+    }
+
+    database = database.map((person) => {
+      if (person.username == username) {
+        return {
+          ...person,
+          money: person.money + amount,
+        };
+      }
+    });
+    callback(null);
+  } else {
+    callback(true);
+  }
+};
+
 function hash(text) {
   return crypto.createHash("sha512").update(text).digest("hex");
+}
+
+// callback (err, price)
+// can be promises or callbacks
+function getQuote(ticker, callback = null) {
+  if (callback) {
+    yahooFinance.quote(
+      { symbol: ticker, modules: ["price"] },
+      (err, quotes) => {
+        let price = quotes.price.regularMarketPrice;
+        if (err) {
+          console.log(err);
+          callback(err, null);
+        } else {
+          callback(null, price);
+        }
+      },
+    );
+  } else {
+    return new Promise((resolve, reject) => {
+      yahooFinance.quote(
+        { symbol: ticker, modules: ["price"] },
+        (err, quotes) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            let price = quotes.price.regularMarketPrice;
+            resolve(price);
+          }
+        },
+      );
+    });
+  }
 }
--- router.js
+++ router.js
@@ -1,10 +1,47 @@
 const express = require("express");
 const DB = require("./db");
 const auth = require("./tokens"); // deals with auth/jwt/tokens
+const yahooFinance = require("yahoo-finance");
+const e = require("express");
+const { v4: uuidv4 } = require("uuid");
 const PATHS = {
   homepage: "/home",
 };
 let router = new express.Router();
+
+// callback (err, price)
+// can be promises or callbacks
+function getQuote(ticker, callback = null) {
+  if (callback) {
+    yahooFinance.quote(
+      { symbol: ticker, modules: ["price"] },
+      (err, quotes) => {
+        if (err) {
+          console.log(err);
+          callback(err, null);
+        } else {
+          let price = quotes.price.regularMarketPrice;
+          callback(null, price);
+        }
+      },
+    );
+  } else {
+    return new Promise((resolve, reject) => {
+      yahooFinance.quote(
+        { symbol: ticker, modules: ["price"] },
+        (err, quotes) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            let price = quotes.price.regularMarketPrice;
+            resolve(price);
+          }
+        },
+      );
+    });
+  }
+}
 
 router.get("/", function (req, res) {
   res.render("index", {
@@ -16,6 +53,44 @@ router.get("/", function (req, res) {
 
 router.get("/logon", (req, res) => {
   res.render("logon", {});
+});
+
+router.get("/purchase", auth.mustBeLoggedIn, async (req, res) => {
+  // The `cart` cookie is a stringified array of cart items.
+  let cart = req.cookies.cart
+    ? JSON.parse(decodeURIComponent(req.cookies.cart))
+    : null;
+
+  for (let i = 0; i < cart.length; i++) {
+    try {
+      cart[i].currentPriceOfOneShare = await getQuote(cart[i].ticker);
+    } catch (err) {
+      console.log(err);
+      cart[i].currentPriceOfOneShare = 0;
+    }
+  }
+
+  let totalPrice = 0;
+  cart.map(
+    (order) => (totalPrice += order.quantity * order.currentPriceOfOneShare),
+  );
+
+  if (totalPrice > req.person.money) {
+    res.redirect(
+      "/buy?alert=You do not have enough funds to complete this transaction.",
+    );
+  } else {
+    // complete purchase
+    DB.completePurchase(req.person.username, cart, totalPrice, (err) => {
+      if (err) {
+        res.redirect("/buy?alert=ERROR: An unexpected error has occured.");
+      } else {
+        // clear cart
+        res.cookie("cart", "");
+        res.redirect("/holdings?alert=Order placed successfully!");
+      }
+    });
+  }
 });
 
 router.post("/logon", (req, res) => {
@@ -48,6 +123,9 @@ router.get("/logout", (req, res) => {
   res.redirect("/logon");
 });
 
+router.get("/holdings", auth.mustBeLoggedIn, (req, res) => {
+  res.render("holdings", { title: "Balance & Holdings", person: req.person });
+});
 router.get(PATHS.homepage, auth.mustBeLoggedIn, (req, res) => {
   // TODO: middleware to check if person is logged on
   // TODO: send all person data in reder method:
@@ -58,8 +136,129 @@ router.get("/signup", (req, res) => {
   res.render("signup", {});
 });
 
-router.get("/checkout", (req, res) => {
-  res.render("checkout", {});
+router.get("/buy", auth.mustBeLoggedIn, async (req, res) => {
+  // The `cart` cookie is a stringified array of cart items.
+  let cart = req.cookies.cart
+    ? JSON.parse(decodeURIComponent(req.cookies.cart))
+    : null;
+
+  if (cart) {
+    for (let i = 0; i < cart.length; i++) {
+      try {
+        cart[i].currentPriceOfOneShare = await getQuote(cart[i].ticker);
+      } catch (err) {
+        console.log(err);
+        cart[i].currentPriceOfOneShare = 0;
+      }
+    }
+    console.log(cart);
+  }
+  res.render("buy", { title: "Buy", cart });
+});
+
+router.post("/updatecart", (req, res) => {
+  let { ticker, quantity, orderType, limitPrice, stopPrice } = req.body;
+
+  getQuote(ticker, (err, price) => {
+    if (err) {
+      res.redirect("/buy?alert=Error: Ticker symbol not found.");
+      return;
+    }
+
+    if (orderType == "limit" && limitPrice < price) {
+      res.redirect(
+        "/buy?alert=Error: Limit order price must be greater than the current price.",
+      );
+      return;
+    } else if (orderType == "stop" && stopPrice > price) {
+      res.redirect(
+        "/buy?alert=Error: Stop order price must be smaller than the current price.",
+      );
+      return;
+    }
+
+    let order = { orderId: uuidv4() };
+    if (orderType == "market") {
+      order = { ...order, ticker, quantity };
+    } else if (orderType == "limit") {
+      order = { ...order, ticker, quantity, limit: limitPrice };
+    } else if (orderType == "stop") {
+      order = { ...order, ticker, quantity, stop: stopPrice };
+    } else {
+      //error
+      res.redirect(
+        "/buy?alert=Error: No order type was selected. This shouldn't happen!",
+      );
+      return;
+    }
+
+    if ("cart" in req.cookies && req.cookies.cart) {
+      // need to decodeURIComponent before JSON parsing it.
+      let currentCartArray = JSON.parse(decodeURIComponent(req.cookies.cart));
+      currentCartArray.push(order);
+      res.cookie("cart", JSON.stringify(currentCartArray), {
+        maxAge: 900000,
+        httpOnly: true,
+      });
+    } else {
+      res.cookie("cart", JSON.stringify([order]), {
+        maxAge: 900000,
+        httpOnly: true,
+      });
+    }
+
+    res.redirect("/buy?alert=Successfully added to cart!");
+  });
+});
+
+// "/quote?ticker=vti"
+router.get("/quote", (req, res) => {
+  let { ticker } = req.query;
+
+  if (!ticker) {
+    res.json({ price: null, err: true });
+  } else {
+    getQuote(ticker, (err, price) => {
+      if (err) {
+        console.log(err);
+        res.json({ price: null, err: true });
+      } else {
+        res.json({ price, err: null });
+      }
+    });
+  }
+});
+
+router.get("/money", auth.mustBeLoggedIn, (req, res) => {
+  res.render("add-money", { title: "Add/Subtract Money", person: req.person });
+});
+
+router.post("/money", auth.mustBeLoggedIn, (req, res) => {
+  let { addOrSubtract, amount } = req.body;
+  DB.addOrSubtractMoney(
+    req.person.username,
+    addOrSubtract,
+    parseInt(amount),
+    (err) => {
+      if (err) {
+        res.send("an error has occured.");
+      } else {
+        res.redirect("/holdings?alert=Account updated successfully.");
+      }
+    },
+  );
+});
+
+router.get("/sell", auth.mustBeLoggedIn, (req, res) => {
+  res.render("sell", { title: "Sell", person: req.person });
+});
+
+router.get("/removecartitem", (req, res) => {
+  let { orderId } = req.query;
+  let cart = JSON.parse(decodeURIComponent(req.cookies.cart));
+  cart = cart.filter((order) => order.orderId != orderId);
+  res.cookie("cart", JSON.stringify(cart), { maxAge: 900000, httpOnly: true });
+  res.redirect("/buy?alert=Item removed from cart.");
 });
 
 router.post("/signup", (req, res) => {
